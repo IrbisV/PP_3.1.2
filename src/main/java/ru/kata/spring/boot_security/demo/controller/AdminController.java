c4ece8c101ad8caf19a5bfc8a3ddf83d28e6fa65
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-10-17 21:28:45 +0300

    fix mapping

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,71 +1,56 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final UserServiceImpl userService;
-    private final PasswordEncoder passwordEncoder;
-    @Autowired
-    public AdminController(UserServiceImpl userService, PasswordEncoder passwordEncoder) {
+
+    public AdminController(UserServiceImpl userService) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/")
-    public String getAllUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
+    @GetMapping()
+    public String getAllUsers(Model model, Principal principal) {
+        User currentUser = userService.findByUsername(principal.getName());
+        model.addAttribute("allUsers", userService.getAllUsers());
+        model.addAttribute("allRoles", userService.getAllRoles());
+        model.addAttribute("newUser", new User());
+        model.addAttribute("currentUser", currentUser);
         return "main-page";
-    }
-
-    @GetMapping("/add-user")
-    public String addUser(Model model) {
-        User user = new User();
-        user.setRoles(new HashSet<>(Arrays.asList(userService.getAllRoles().get(0))));
-        model.addAttribute("roles", userService.getAllRoles());
-        model.addAttribute("newUser", user);
-        return "user-edit-page";
-    }
-
-    @GetMapping("/user-info/{id}")
-    public String userInfo(@PathVariable("id") long id, Model model) {
-        User currentUser = userService.getUser(id);
-        model.addAttribute("userInfo", currentUser);
-        return "user-page";
     }
 
     @PostMapping()
     public String createUser(@ModelAttribute("newUser") User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.save(user);
         return "redirect:/admin/";
     }
 
-    @RequestMapping("/user-delete/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin/";
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") long id, Principal principal, HttpServletRequest request) throws ServletException {
+        if (userService.findByUsername(principal.getName()).getId() == id) {
+            userService.deleteUser(id);
+            request.logout();
+            return "redirect:/login";
+        } else {
+            userService.deleteUser(id);
+            return "redirect:/admin/";
+        }
     }
 
-    @RequestMapping("/update-info/{id}")
-    public String userInfo(Model model, @PathVariable("id") long id) {
-        User currentUser = userService.getUser(id);
-        Set<Role> roles = new HashSet<>();
-        model.addAttribute("roles", userService.getAllRoles());
-        model.addAttribute("newUser", currentUser);
-        return "user-edit-page";
+    @PatchMapping("/{id}")
+    public String editUser(@PathVariable("id") long id, @ModelAttribute("editUser") User editUser) {
+        userService.editUser(id, editUser);
+        return "redirect:/admin/";
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,27 +1,40 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @Controller
 public class UserController {
     private final UserServiceImpl userService;
 
-    @Autowired
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
     @GetMapping("/user")
-    public String updateUser(Model model, Principal principal) {
-        User currentUser = userService.findByUsername(principal.getName());
-        model.addAttribute("userInfo", currentUser);
-        return "user-page";
+    public String userInfo(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("currentUser", user);
+        return "user";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/manualLogout")
+    public String customLogut(Model models, HttpServletRequest request) throws ServletException
+    {
+        request.logout();
+        return "redirect:/login";
     }
 }
