c3dfcf639b9ece757b21d138ad02ad1b03fe7382
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-10-19 16:03:58 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,20 +1,20 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @Controller
 public class UserController {
     private final UserServiceImpl userService;
 
+    @Autowired
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
@@ -31,10 +31,8 @@ public class UserController {
         return "login";
     }
 
-    @PostMapping("/manualLogout")
-    public String customLogut(Model models, HttpServletRequest request) throws ServletException
-    {
-        request.logout();
-        return "redirect:/login";
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
     }
 }
